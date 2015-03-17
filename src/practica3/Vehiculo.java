597764dfe7dbee1e1f8c78936751ed32d156c966
Sergio <wikigdl@gmail.com>
2015-03-16 23:27:36 -0600

    almost finish

--- src/practica3/Vehiculo.java
+++ src/practica3/Vehiculo.java
@@ -4,10 +4,25 @@ package practica3;
  * Created by sheko on 16/03/15.
  */
 
+import java.text.ParseException;
 import java.util.*;
 
 public class Vehiculo {
     private String nombre;
+    private String soy;
+    private String valor;
+    private Hashtable<String, String> tabla
+            = new Hashtable<String, String>();
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+
 
     public String getSoy() {
         return soy;
@@ -17,23 +32,25 @@ public class Vehiculo {
         this.soy = soy;
     }
 
-    private String soy;
-    private Hashtable<String, String> tabla
-            = new Hashtable<String, String>();
+
     public Vehiculo(String Nombre){
         this.nombre=Nombre;
     }
     public void setValue(String key, String value){
         this.tabla.put(key, value);
     }
+
     public boolean containsKey(String key){
-        return this.containsKey(key);
+        return this.tabla.containsKey(key);
+    }
+    public String getValue(String key){
+        return this.tabla.get(key);
     }
     public String getNombre(){
         return nombre;
     }
     public HashMap isPosible(HashMap properties){
-        boolean result=false;
+        boolean result=true;
         HashMap map= new HashMap();
         List<String> found =new ArrayList();
         List<String> notFound =new ArrayList();
@@ -43,7 +60,8 @@ public class Vehiculo {
             String key = name.toString();
             if (this.tabla.containsKey(key)) {
                 String value = properties.get(name).toString();
-                System.out.println(key + " " + value);
+
+                boolean rest = evaluar(key, value);
                 //funcion evaluar
                 found.add(key);
 
@@ -59,5 +77,28 @@ public class Vehiculo {
         }
         return map;
 
+    }
+    boolean evaluar(String key,String inputVal){
+        boolean val=false;
+        String sentence =this.tabla.get(key);
+        String operador;
+        operador = sentence.substring(0,1);
+        sentence =sentence.substring(1);
+
+        if(operador.equals("="))
+            if(inputVal.trim().equals(sentence.trim()))
+                val= true;
+        if(operador.equals("<"))
+            try{
+                if(Integer.parseInt(inputVal)<Integer.parseInt(sentence))
+                    val =true;
+            }catch(NumberFormatException nfe){}
+        if(operador.equals(">"))
+            try{
+                if(Integer.parseInt(inputVal)<Integer.parseInt(sentence))
+                    val =true;
+            }catch(NumberFormatException nfe){}
+
+        return val;
     }
 }
--- src/practica3/main.java
+++ src/practica3/main.java
@@ -19,6 +19,8 @@ public class main {
     public static void main(String[] args) {
         Hashtable<String, String> tabla = new Hashtable<String, String>();
         List<Vehiculo> objetos = new ArrayList();
+        List<Vehiculo> posibles= new ArrayList();
+        List<HashMap>pMap=new ArrayList();
         String[] llavesPrimarias = { "vihicleType", "Numberweels", "motor" };
         String[] valores = { "1", "2", "3" };
         String[] vehiculos = { "moto", "lamelarene", "lamelacheco" };
@@ -66,6 +68,47 @@ public class main {
 
 
         }
+        //buscamos propiedades dentro de todos los objetos creados
+        for (Vehiculo actual : objetos) {
+            HashMap rBuesqueda = actual.isPosible(pBusqueda);
+                if (!rBuesqueda.isEmpty()){
+                    posibles.add(actual);
+                    pMap.add(rBuesqueda);
+                }
+
+        }
+        if (posibles.size()<0){
+            if(posibles.size()==1){
+                Vehiculo vFinal = posibles.get(0);
+                JOptionPane.showMessageDialog(null,"El programa tiene una respuesta a esas caracteristicas, es un:"+ vFinal.getNombre());
+            } else{
+                JOptionPane.showMessageDialog(null,"El programa no tiene una respuesta a esas caracteristicas muchas posibilidades encontradas");
+                /*
+
+                funcion para reducir el numero de posibilidades buscando un padre del cual heredar posibilidades
+
+                for(int j=0;j<posibles.size();j++){
+                    Vehiculo actual = posibles.get(j);
+                    HashMap aProp= pMap.get(j);
+                    List <String>notFound = (List) aProp.get("notFound");
+                    for(int i=0;j<posibles.size();i++){
+                        if(i!=j){
+                            Vehiculo father = posibles.get(i);
+                            for(String prop:notFound){
+                                if(father.getSoy().equals(prop))
+                                    if(father.getValor().equals())
+                            }
+                        }
+
+                    }
+
+
+                }*/
+            }
+
+        }else
+            JOptionPane.showMessageDialog(null,"El programa no tiene una respuesta a esas caracteristicas");
+
 
 
     }
